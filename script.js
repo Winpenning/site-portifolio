3c397e7fe08c05b0c654e15eee7229cdc5faf4e5
winpenning <zs.phenriqueprofessional@gmail.com>
2023-05-21 11:22:36 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-
 const titulo = document.querySelector('.digitando');
+
 function ativaletra(elemento) {
     const arrTexto = elemento.innerHTML.split('');
     elemento.innerHTML = '';
@@ -10,18 +10,16 @@ function ativaletra(elemento) {
     });
 }
 
-
 function menuMobile() {
     const activeMenu = document.querySelector('.fa-bars');
     const navMenu = document.querySelector('header .navegacao-primaria');
-
-
     activeMenu.addEventListener('click', () => {
         activeMenu.classList.toggle('fa-x');
         navMenu.classList.toggle('ativado');
-    });
 
+    });
 }
+
 ativaletra(titulo);
 menuMobile();
 
